244ffda194d67d03617c97b15f676a7f281621ae
Pratyush Dev <fate17x@gmail.com>
2026-02-05 17:40:43 +0530

    final push 5/2

---
+++ py_ml/ml.py
@@ -0,0 +1,17 @@
+#check out np.mean np.median and stats.mode, easy. also, checkout, np.percentile(arr,%ile) #returns the arr value at that %ile
+#check out np.random.normal(mean,std,size) distribution and plt.hist(arr,no.of.bars). also you alr know plt.scatter(x,y)
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#prereq (using random.normal and scatter)
+"""x=np.random.normal(5,2,1000) #np.random.normal(mean,std,size)
+y=np.random.normal(100,10,1000)
+
+plt.scatter(x,y)
+plt.xlabel("x axis (maybe age of car)")
+plt.ylabel("y axis (maybe speed of car)")
+plt.show()
+"""
+
+
