229cb792040bb7867f6e6709f3f3e79980f9bc3b
Pratyush Dev <fate17x@gmail.com>
2026-02-10 00:41:26 +0530

    poly regress.1

--- py_ml/ml.py
+++ py_ml/ml.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy import stats
 
 #prereq (using random.normal and scatter)
 """x=np.random.normal(5,2,1000) #np.random.normal(mean,std,size)
@@ -14,4 +15,32 @@ plt.ylabel("y axis (maybe speed of car)")
 plt.show()
 """
 
+"""#linear regression (relation between data points)
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
+slope,intercept,r,p,stdErr=stats.linregress(x,y) 
+#r is the coefficient of correlation. r range: (-1,1), 0 means no relation (chopped), 1/-1 means 100% related.
+print(r) #here, -0.76, there is a relation but not strong. can be used for linregress tho.
+
+def regPoints(x):
+    return slope*x+intercept
+
+regressionLine=list(map(regPoints,x))
+
+#prediction test
+print("speed of a car 10yo as predicted is: ",regPoints(7))
+
+plt.scatter(x,y)
+plt.plot(x,regressionLine)
+plt.xlabel("age")
+plt.ylabel("speed")
+plt.grid()
+plt.show()"""
+
+#polynomial regression (ahhhh we curving polynomially now)
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+plt.scatter(x, y)
+plt.show()
