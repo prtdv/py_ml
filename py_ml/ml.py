eb7e953048b7a05910e47498b2b5268520bc68b9
Pratyush Dev <fate17x@gmail.com>
2026-02-19 02:10:20 +0530

    completed logistic regression

--- py_ml/ml.py
+++ py_ml/ml.py
@@ -256,9 +256,9 @@ print(odds) # [4.03541657], it means if the size of a tumor increases by 1mm the
 
 #probability, inside maths of logr.predict()
 def logit2prob(logistic_reg, X):
-  log_odds = logistic_reg.coef_ * X + logistic_reg.intercept_ #log_odds= log(p / (1-p))
-  odds = np.exp(log_odds) #removing log, odds= p / (1-p)
-  probability = odds / (1 + odds)
+  log_odds = logistic_reg.coef_ * X + logistic_reg.intercept_ #log_odds= log(p / (1-p)), eq1
+  odds = np.exp(log_odds) #removing log, odds= p / (1-p), eq2
+  probability = odds / (1 + odds) #after doing some maths in eq2, we get p like this
   return(probability)
 
 print(logit2prob(logistic_reg, X)) #prints the probabilities of each X being cancerous (y)
